97bbba677e1b23cbcfa4e2fd1d48b9f7d8781029
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-17 13:42:04 +0200

    About section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 
 export default function App() {
   return (
     <section className="bg-background min-h-screen bg-no-repeat bg-center bg-cover ">
       <Hero />
+      <About />
     </section>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,34 @@
+const About = () => {
+  return (
+    <section className="h-screen relative flex items-center ">
+      <section className="hidden md:block h-full w-[50%] border-2">
+        <video
+          src="3.mp4"
+          autoPlay
+          loop
+          muted
+          className="h-full w-full object-cover"
+        ></video>
+      </section>
+      <section className="h-full w-full md:hidden">
+        <img
+          src="about.jpg"
+          alt=""
+          className="w-full h-full object-cover object-center"
+        />
+      </section>
+      <section className="absolute text-white font-ubuntuMono p-5 w-[90%] md:relative md:w-[40%] mx-auto ">
+        <h2 className="text-5xl uppercase tracking-wider mb-5">Immergence</h2>
+        <p className="text-xl text-justify">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis nam
+          iste saepe rerum a eius, ex mollitia quasi at similique, et suscipit
+          fugiat non rem, aliquam facere unde aspernatur error molestiae? Dolor
+          rem laudantium suscipit, maxime soluta ab sint nobis quaerat similique
+          cupiditate quam voluptatem aspernatur cumque dicta voluptas assumenda.
+        </p>
+      </section>
+    </section>
+  );
+};
+
+export default About;
