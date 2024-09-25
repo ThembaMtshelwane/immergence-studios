2ea1b1be3400608dca934be213f58d712e7aed8a
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-25 19:47:08 +0200

    Footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className=" mt-20 backdrop-blur-sm">
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8 sm:w-[80%]">
+      <div className="w-full p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
             href="#"
@@ -38,9 +38,10 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <h3 className="text-2xl mb-4">Disclaimer</h3>
         <p className="mb-4 text-xl text-justify">
-          I do not own any of the images, videos ang gifs used in this website.
-          This website is not for commercial use. It is purely to demonstrate
-          frontend capabilities. Credit for the visual content is listed in the{" "}
+          I do not own any of the images, videos, gifs and audio used in this
+          website. This website is not for commercial use. It is purely to
+          demonstrate frontend capabilities. Credit for the visual content is
+          listed in the{" "}
           <a
             href="https://github.com/ThembaMtshelwane/immergence-studios"
             className="text-red-600"
