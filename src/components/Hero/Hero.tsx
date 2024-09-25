ad965712e9fcc26051a638e93af3f11bf38265c8
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-25 19:43:41 +0200

    Hero: text re-adjusted

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -9,14 +9,14 @@ const Hero = () => {
         playsInline
         className="h-full w-full object-cover object-center"
       />
-      <h1 className="absolute h-[50%] w-[90%] grid lg:gap-20 items-center justify-center text-center grid-rows-6 text-6xl font-ubuntuMono sm:text-7xl lg:text-9xl uppercase ">
-        <span className="animate-slidein300 opacity-0  row-[2/4] md:row-[2/4]">
+      <h1 className="absolute h-full  w-[90%] grid  items-center justify-center text-center grid-rows-12 text-6xl font-ubuntuMono sm:text-7xl md:text-8xl lg:text-[130px] uppercase ">
+        <span className="animate-slidein300 opacity-0  row-[2/4] md:row-[4/5]">
           Welcome
         </span>
-        <span className="animate-slidein500 opacity-0  row-[4/5] md:row-[4/5]">
+        <span className="animate-slidein500 opacity-0  row-[4/5] md:row-[6/7] lg:row-[6/8]">
           to
         </span>
-        <p className="text-glitch  row-[5/6] md:row-[5/7]">
+        <p className="text-glitch  row-[5/7] md:row-[8/9] lg:row-[9/10]">
           <span className="animate-slidein700 opacity-0 ">IMMERGENCE</span>
         </p>
       </h1>
